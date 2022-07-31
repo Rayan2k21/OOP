bce28e12e342c223986a4ac085bb73b908c1855e
Rayan AHmad <105577706+Rayan2k21@users.noreply.github.com>
2022-07-31 11:18:00 +0500

    Create ArrayMethod.java

---
+++ ArrayMethod.java
@@ -0,0 +1,37 @@
+class Array
+
+{
+
+    void display(int a[])
+
+ { 
+
+      for(int i=0; i<5; i++)
+
+     {
+
+           System.out.println(a[i]);
+
+      } 
+
+   }
+
+}
+
+public class ArrayMethod
+
+{
+
+   public static void main (String args[])
+
+{
+
+  int x[]={20,30,40,50,60};
+
+  Array obj.new Array();
+
+  obj.display(x);
+
+  }
+
+}
