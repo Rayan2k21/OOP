8c2927457d6f450149bf810f01444fa4da2dd3e7
Rayan AHmad <105577706+Rayan2k21@users.noreply.github.com>
2022-07-31 11:18:42 +0500

    Create 2DArray.java

---
+++ 2DArray.java
@@ -0,0 +1,29 @@
+public class 2DArray
+
+{
+
+   public static void main (String args[])
+
+{
+
+  int x[][]=new int[][];
+
+  x[0][0]=50;
+
+  x[0][1]=60;
+
+  x[1][0]=70;
+
+  x[1][1]=50;
+
+  System.out.println(x[0][0]);
+
+  System.out.println(x[0][1]);
+
+  System.out.println(x[1][0]);
+
+  System.out.println(x[1][1]);
+
+  }
+
+}
