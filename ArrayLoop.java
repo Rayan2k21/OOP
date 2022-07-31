5db3e3438d6b64e693b057d6fe46256b6c52b96a
Rayan AHmad <105577706+Rayan2k21@users.noreply.github.com>
2022-07-31 11:17:18 +0500

    Create ArrayLoop.java

---
+++ ArrayLoop.java
@@ -0,0 +1,21 @@
+public class ArrayLoop
+
+{
+
+   public static void main (String args[])
+
+{
+
+  int x[]={20,30,40,50,60};
+
+  for(int i=0; i<5; i++)
+
+{
+
+    System.out.println(x[i]);
+
+    }
+
+  }
+
+}
