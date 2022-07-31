5a98e6339577bff3cad07abceb9a2be9a054c64b
Rayan AHmad <105577706+Rayan2k21@users.noreply.github.com>
2022-07-31 11:19:20 +0500

    Create 2DArrayLoop.java

---
+++ 2DArrayLoop.java
@@ -0,0 +1,23 @@
+public class 2DArrayLoop
+
+{
+
+   public static void main (String args[])
+
+{
+
+  int x[][]={ {50,60},{70,80} };
+
+  for(int row=0; row<5; row++)
+
+ {
+
+     for(int col=0; col<5; col++)
+
+   {
+
+  System.out.println(x[row][col]);
+
+  }
+
+}
