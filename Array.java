267ea0ee7201d4927fa4c79437370dd958037dae
Rayan AHmad <105577706+Rayan2k21@users.noreply.github.com>
2022-07-31 11:16:31 +0500

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,33 @@
+public class testarray
+
+{
+
+   public static void main (String args[])
+
+{
+
+  int x[]=new int[5];
+
+  x[0]=20;
+
+  x[1]=30;
+
+  x[2]=40;
+
+  x[3]=50;
+
+  x[4]=60;
+
+  System.out.println(x[0]);
+
+  System.out.println(x[1]);
+
+  System.out.println(x[2]);
+
+  System.out.println(x[3]);
+
+  System.out.println(x[4]);
+
+}
+
+}
